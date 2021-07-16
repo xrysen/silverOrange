534e327acda9dc9d42dea428340456c12746224a
Sean Oyler <xrysen@gmail.com>
2021-07-16 14:08:41 -0700

    Removes unneeded markdown component in summary

--- web/src/Components/PostItem.jsx
+++ web/src/Components/PostItem.jsx
@@ -1,11 +1,9 @@
-import ReactMarkdown from "react-markdown";
-
 export default function PostItem(props) {
   return (
     <tr>
       <td>{props.title}</td>
       <td>{props.author}</td>
-      <td><ReactMarkdown>{props.summary.replace(/#/g, '').replace(/\n/g, '').substring(0, 60) + "..."}</ReactMarkdown></td>
+      <td>{props.summary.replace(/#/g, '').replace(/\n/g, '').substring(0, 60) + "..."}</td>
       <td>{new Date(props.date).toISOString().substring(0, 10)}</td>
     </tr>
   );
